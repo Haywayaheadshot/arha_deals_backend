b9802f477dea49e2dfb1c8027401ebe1f4c8661e
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-20 01:20:50 +0000

    Add jwt to app

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  devise_for :uses
   resources :phones
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
