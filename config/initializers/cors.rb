0ec469c1d83af6045525c59cd7abc2ef3acc3799
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-24 00:09:08 +0000

    rewrite cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-     origins '*'
-    #  origins ['your-frontend-url']
-     resource '*',
-              headers: :any,
-              expose: %w(Authorization),
-              methods: %i[get post put patch delete head options]
+      origins '*'
+      resource '*',
+        headers: :any,
+        expose: ['Authorization'],
+        methods: [:get, :post, :put, :patch, :delete, :head, :options]
     end
-end
+  end
+  
