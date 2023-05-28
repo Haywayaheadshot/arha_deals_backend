dbbd5838b69a88d2e864af6cfa6cbe42d3450eec
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-05-28 23:20:45 +0000

    add columns to create phones

--- db/migrate/20230519152757_create_phones.rb
+++ db/migrate/20230519152757_create_phones.rb
@@ -1,6 +1,13 @@
 class CreatePhones < ActiveRecord::Migration[7.0]
   def change
     create_table :phones do |t|
+      t.string :name
+      t.integer :amount
+      t.integer :stock
+      t.string :images_src, array: true, default: []
+      t.text :description
+      t.string :condition
+      t.string :video_src
 
       t.timestamps
     end
