4b99c35eeb06d62a0401278a40b15caf3d06cdb0
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-06-09 01:50:34 +0000

    rails generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_005553) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_08_032525) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "phone_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["phone_id"], name: "index_cart_items_on_phone_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "cart_item_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_item_id"], name: "index_carts_on_cart_item_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "phones", force: :cascade do |t|
     t.string "name"
@@ -49,4 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_005553) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cart_items", "phones"
+  add_foreign_key "carts", "cart_items"
+  add_foreign_key "carts", "users"
 end
