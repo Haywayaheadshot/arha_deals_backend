f1dd73799e6fda2dc80b60306675d56069d8dacf
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-06-11 00:30:46 +0000

    add api route to delete item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
     # API endpoint for adding items to the cart
     post "/addtocart", to: "carts#add"
+
+    # API endpoint for removing items to the cart
+    delete "/deletefromcart", to: "carts#delete"
   end
 
   # Defines the root path route ("/")
