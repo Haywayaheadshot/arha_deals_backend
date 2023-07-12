7a1266d2e1de161609c16739d98128efba346ca9
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-12 09:59:49 +0000

    change BabyProducts to BabyProduct

--- app/controllers/api/baby_products_controller.rb
+++ app/controllers/api/baby_products_controller.rb
@@ -1,6 +1,7 @@
 class Api::BabyProductsController < ApplicationController
+
   def index
-    @baby_products = BabyProducts.all
+    @baby_products = BabyProduct.all
     render json: @baby_products,
            only: %i[id name amount stock images_src specs condition video_src]
   end
