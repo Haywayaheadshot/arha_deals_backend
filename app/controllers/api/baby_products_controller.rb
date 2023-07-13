19c937ea7fef1618aa1d92e16bb6447da98aba8d
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-13 05:09:57 +0000

    Send all baby products column

--- app/controllers/api/baby_products_controller.rb
+++ app/controllers/api/baby_products_controller.rb
@@ -2,6 +2,6 @@ class Api::BabyProductsController < ApplicationController
   def index
     @baby_products = BabyProduct.all
     render json: @baby_products,
-           only: %i[id name amount stock images_src specs condition video_src]
+           only: %i[id name amount stock images_src specs condition video_src productHightlights features]
   end
 end
